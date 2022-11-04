1936935c670f7a19f721118b0d0f78bf30e2108f
Cupskrrt <khanalfarizzi@hotmail.com>
2022-11-05 01:57:19 +0700

    commit au keberapa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
-import ContactApp from './contact-app/ContactApp'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './movie-app/components/Navigation';
+import HomePage from './movie-app/pages/HomePage';
+import NowPlayingPage from './movie-app/pages/NowPlayingPage';
+import SearchPageWrapper from './movie-app/pages/SearchPage';
+import UpcomingPage from './movie-app/pages/UpcomingPage';
+import DetailPageWrapper from './movie-app/pages/DetailPage';
 
 function App() {
   return (
-   <ContactApp/>
+    <>
+    <header>
+      <h1>Movie Catalogue</h1>
+      <Navigation />
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/now-playing" element={<NowPlayingPage />} />
+        <Route path="/upcoming" element={<UpcomingPage />} />
+        <Route path="/search" element={<SearchPageWrapper />} />
+        <Route path="/movies/:id" element={<DetailPageWrapper />} />
+      </Routes>
+    </main>
+  </>
   );
 }
 
---
+++ src/movie-app/pages/DetailPage.jsx
@@ -0,0 +1,35 @@
+import { get } from "prompt";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import MovieDetail from "../components/MovieDetails";
+import { getMovie } from "../utils/data";
+
+function DetailPageWrapper() {
+  const { id } = useParams();
+  return <DetailPage id={Number(id)} />;
+}
+
+// class DetailPage extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       movie: getMovie(props.id)
+//     }
+//   }
+
+//   render() {
+//     if (this.state.movie === null) {
+//       return <p>Movie is not found!</p>;
+//     }
+
+//     return (
+//       <section>
+//         <MovieDetail {...this.state.movie} />
+//       </section>
+//     );
+//   }
+// }
+
+export default DetailPageWrapper;
